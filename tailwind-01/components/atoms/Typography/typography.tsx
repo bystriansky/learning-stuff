c3e16ff3e5ccf8e2d329343892502b2c53c4fe51
bystriansky <bystriansky.d@gmail.com>
2022-07-11 16:37:00 +0200

    wip: header section

--- tailwind-01/components/atoms/Typography/typography.tsx
+++ tailwind-01/components/atoms/Typography/typography.tsx
@@ -1,1 +1,23 @@
-export const Typography = () => <div>aaa</div>
+import { ReactNode } from 'react'
+
+type Props = {
+  children: ReactNode
+  variant?: 'h1' | 'h2' | 'div'
+}
+
+const variantMapping = {
+  h1: 'h1',
+  h2: 'h2',
+  h3: 'h3',
+}
+
+export const Typography = ({ children, variant = 'div' }: Props) => {
+  switch (variant) {
+    case 'h1':
+      return <h1 className="text-6xl font-extrabold">{children}</h1>
+    case 'h2':
+      return <h2>{children}</h2>
+    default:
+      return <div>{children}</div>
+  }
+}
--- tailwind-01/pages/index.tsx
+++ tailwind-01/pages/index.tsx
@@ -1,84 +1,79 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="bg-primary-main min-h-screen max-w-screen-lg py m-auto">
       <Head>
-        <title>Create Next App</title>
+        <title>rite.</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-sm font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
+      <header className="text-gray-300 text-sm flex items-center justify-between px-12 py-5">
+        <ul className="flex gap-8">
+          <li className="font-medium">Features</li>
+          <li className="font-medium">Pricing & Plans</li>
+        </ul>
+        <div className="text-3xl font-medium">rite.</div>
+        {/*  TODO: dropdown */}
+        <Link href="/">
+          <a className="border-2 px-7 py-2" href="#">
+            Sing Up
           </a>
-        </h1>
+        </Link>
+      </header>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+      <main>
+        <section className="px-20 py-20">
+          <h1 className="w-3/4 mb-10 text-8xl leading-none font-semibold">
+            Manage Your Big Project.
+          </h1>
+          <p className="w-1/2 mb-10 text-gray-300">
+            Intuitive features give any team the ability to quickly set up and
+            customize workflows productive team.
+          </p>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
+          <Link href="/">
+            <a href="/" className="flex items-center text-3xl">
+              <span className="mr-5">Get Started</span>
+              <div className="relative overflow-hidden flex items-center w-10 h-10 bg-blue-900 rounded-full">
+                <div className="w-8">
+                  <svg className="fill-current" viewBox="0 0 20 20">
+                    <path
+                      d="M1.729,9.212h14.656l-4.184-4.184c-0.307-0.306-0.307-0.801,0-1.107c0.305-0.306,0.801-0.306,1.106,0
+	l5.481,5.482c0.018,0.014,0.037,0.019,0.053,0.034c0.181,0.181,0.242,0.425,0.209,0.66c-0.004,0.038-0.012,0.071-0.021,0.109
+	c-0.028,0.098-0.075,0.188-0.143,0.271c-0.021,0.026-0.021,0.061-0.045,0.085c-0.015,0.016-0.034,0.02-0.051,0.033l-5.483,5.483
+	c-0.306,0.307-0.802,0.307-1.106,0c-0.307-0.305-0.307-0.801,0-1.105l4.184-4.185H1.729c-0.436,0-0.788-0.353-0.788-0.788
+	S1.293,9.212,1.729,9.212z"
+                    ></path>
+                  </svg>
+                </div>
+              </div>
+            </a>
+          </Link>
+        </section>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <section className="flex text-4xl justify-around px-20 bg-gray-800">
+          <div className="text-center">
+            <div>49k +</div>
+            <div>Users</div>
+          </div>
+          <div>
+            <div>2.3k +</div>
+            <div>Subscriber</div>
+          </div>
+          <div>
+            <div>198 +</div>
+            <div>Country</div>
+          </div>
+          <div>
+            <div>76 %</div>
+            <div>Profit</div>
+          </div>
+        </section>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
   )
 }
